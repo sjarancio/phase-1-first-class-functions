703aa5087484e8a6ed5ce4cf5217e63aeb9a91b3
Stephen Arancio <sjarancio@gmail.com>
2021-06-29 17:55:19 -0400

    commit tree 2ccaedabbf1440bd73222737c26eb276225cf6d6

---
+++ index.js
@@ -0,0 +1,11 @@
+let callback = smile => {return smile}
+
+function receivesAFunction(callback){
+    return callback
+}
+// receivesAFunction(returnsANamedFunction);
+
+
+function returnsANamedFunction(){
+
+}
